eba5caf932936f207a3c675dc84b460d722ae87a
Rama <hidayahweb@gmail.com>
2021-05-11 21:01:55 +0700

    kajs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from _typeshed import OpenTextModeUpdating
 from typing import Optional
 
 from fastapi import FastAPI
@@ -9,6 +10,6 @@ def read_root():
     return {"Hello": "World1", "this":"from github"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/{item_id}-{nama}")
+def read_item(item_id: int, q: Optional[str] = None, name: Optional[str]=None):
+    return {"item_id": item_id, "q": q, "say": "halo {nama}"}
